1ca90c0d24ec9cfb86bea23bec9797f5b73328c1
Adam Nofsinger <adamnoffie@gmail.com>
2010-03-12 16:11:10 -0600

    ADDED: BCrypter project

---
+++ Projects/BCrypter/BCrypter/FormMain.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace BCrypter
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGo = new System.Windows.Forms.Button();
+            this.txtToCrypt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.udSaltGenPasses = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.udSaltGenPasses)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(76, 41);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 0;
+            this.btnGo.Text = "BCrypt!";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // txtToCrypt
+            // 
+            this.txtToCrypt.Location = new System.Drawing.Point(76, 15);
+            this.txtToCrypt.Name = "txtToCrypt";
+            this.txtToCrypt.Size = new System.Drawing.Size(378, 20);
+            this.txtToCrypt.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "To BCrypt:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(460, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Salt Gen passes:";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(76, 70);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(517, 20);
+            this.txtResult.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Result:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // udSaltGenPasses
+            // 
+            this.udSaltGenPasses.Location = new System.Drawing.Point(551, 16);
+            this.udSaltGenPasses.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.udSaltGenPasses.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.udSaltGenPasses.Name = "udSaltGenPasses";
+            this.udSaltGenPasses.Size = new System.Drawing.Size(42, 20);
+            this.udSaltGenPasses.TabIndex = 7;
+            this.udSaltGenPasses.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 121);
+            this.Controls.Add(this.udSaltGenPasses);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtToCrypt);
+            this.Controls.Add(this.btnGo);
+            this.Name = "frmMain";
+            this.Text = "BCrypter 0.1";
+            ((System.ComponentModel.ISupportInitialize)(this.udSaltGenPasses)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox txtToCrypt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown udSaltGenPasses;
+    }
+}
+
---
+++ Projects/BCrypter/BCrypter/FormMain.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BCrypter
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            txtResult.Text = 
+                BCrypt.HashPassword(txtToCrypt.Text, BCrypt.GenerateSalt((int)udSaltGenPasses.Value));
+        }
+    }
+}
